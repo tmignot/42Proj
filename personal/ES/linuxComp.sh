54a6b6ffd11f45790abdd1c244ad246b50529954
tmignot <tmignot@student.42.fr>
2015-03-27 04:35:59 +0100

    better compscripts

--- personal/ES/linuxComp.sh
+++ personal/ES/linuxComp.sh
@@ -1,6 +1,11 @@
+for f in  Makefile *.so *.bundle *.o
+do
+	rm $f 2>/dev/null
+done
+
 swig3.0 -ruby swigWrapper.i &&
-	gcc -c -fpic swigWrapper_wrap.c main.c -I /usr/local/include/ruby-2.1.0/x86_64-linux/ -I /usr/local/include/ruby-2.1.0/ &&
-	gcc -shared swigWrapper_wrap.o main.o -o threadlaunch.so &&
-	ruby extconf.rb &&
-	make &&
-	sudo make install
+gcc -c -fpic swigWrapper_wrap.c main.c -I /usr/local/include/ruby-2.1.0/x86_64-linux/ -I /usr/local/include/ruby-2.1.0/ &&
+gcc -shared swigWrapper_wrap.o main.o -o threadlaunch.so &&
+ruby extconf.rb &&
+make &&
+sudo make install
--- personal/ES/macosxComp.sh
+++ personal/ES/macosxComp.sh
@@ -1,6 +1,13 @@
+for f in  *.o *.bundle *.so Makefile
+do
+	rm $f 2> /dev/null
+done
+
+
 swig -ruby swigWrapper.i &&
 gcc -c -fpic swigWrapper_wrap.c main.c -I $HOME/.brew/Cellar/ruby/2.2.0/include/ruby-2.2.0/x86_64-darwin14 \
 									   -I $HOME/.brew/Cellar/ruby/2.2.0/include/ruby-2.2.0 &&
 gcc -bundle -flat_namespace -undefined suppress swigWrapper_wrap.o main.o -o threadlaunch.bundle &&
-ruby extconf.rb &&
-make
+$HOME/.brew/Cellar/ruby/2.2.0/bin/ruby extconf.rb &&
+make &&
+make install
